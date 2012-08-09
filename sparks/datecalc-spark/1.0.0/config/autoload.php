550bdb3d49149cf088ddd966592027eb6b863dfa
Jan Lindblom <lindblom.jan@gmail.com>
2012-08-09 16:45:55 +0200

    Adding autoload entries for the sparks.

---
+++ sparks/datecalc-spark/1.0.0/config/autoload.php
@@ -0,0 +1,10 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @author	Jan Lindblom <jan@powcorp.se>
+ * @copyright	Copyright (c) 2012, POW! Corp.
+ * @license	MIT
+ * @version	1.0
+ */
+
+$autoload['helper'] = array('datecalc');
